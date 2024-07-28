e69b84fcba90d5e76e19f90637ff238af23804fa
wutao <497863986@qq.com>
2024-07-28 09:45:12 +0800

    add full proxy in bash

--- bash/proxy_with_arg.sh
+++ bash/proxy_with_arg.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-export http_proxy="http://127.0.0.1:1087"
-export https_proxy="http://127.0.0.1:1087"
+PROXY=http://172.16.8.172:10089
+
+export ALL_PROXY=$PROXY
+export all_proxy=$PROXY
+
+export HTTP_PROXY=$PROXY
+export http_proxy=$PROXY
+
+export HTTPS_PROXY=$PROXY
+export https_proxy=$PROXY
+
+export SOCKS_PROXY=$PROXY
+export socks_proxy=$PROXY
+
+export FTP_PROXY=$PROXY
+export ftp_proxy=$PROXY
+
+export TELNET_PROXY=$PROXY
+export telnet_proxy=$PROXY
+
+export RSYNC_PROXY=$PROXY
+export rsync_proxy=$PROXY
 
 args=("$@")
 
--- linux/proxy_with_arg_home.sh
+++ linux/proxy_with_arg_home.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-export ALL_PROXY="http://172.16.8.172:10089"
+PROXY=http://172.16.8.172:10089
+
+export ALL_PROXY=$PROXY
+export all_proxy=$PROXY
+
+export HTTP_PROXY=$PROXY
+export http_proxy=$PROXY
+
+export HTTPS_PROXY=$PROXY
+export https_proxy=$PROXY
+
+export SOCKS_PROXY=$PROXY
+export socks_proxy=$PROXY
+
+export FTP_PROXY=$PROXY
+export ftp_proxy=$PROXY
+
+export TELNET_PROXY=$PROXY
+export telnet_proxy=$PROXY
+
+export RSYNC_PROXY=$PROXY
+export rsync_proxy=$PROXY
 
 args=("$@")
 
