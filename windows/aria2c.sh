a741489073fdf2b0ed5477e19dbc69fa4d66fb53
Tao Wu <Tao.Wu@microchip.com>
2025-07-03 10:35:03 +0800

    windows: update cmd input in aria2c.sh

--- windows/aria2c.sh
+++ windows/aria2c.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-aria2c -i New\ Text\ Document.txt -j 5 --all-proxy=http://172.16.63.20:10089
+input_file="New\ Text\ Document.txt"
+all_proxy="http://172.16.63.20:10089"
+job=6
+
+# parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -i|--input)
+            input_file="$2"
+            shift 2
+            ;;
+				--all-proxy)
+            all_proxy="$2"
+            shift 2
+            ;;
+				-j)
+            job="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+aria2c -i $input_file -j $job --all-proxy=$all_proxy
