3f53298b2d796eadd968255df28e99e0b5aa8e9d
Tao Wu <Tao.Wu@microchip.com>
2025-07-03 11:14:09 +0800

    linux: add cpufreq set with ondemand, powersave, performance, schedutil governor

---
+++ linux/cpufreq/cpufreq-set-ondemand.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cpu_core=8
+
+# parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -n|--core-num)
+            cpu_core="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+for ((i = 0; i < $cpu_core; i++)); do
+    /usr/bin/cpufreq-set -c $i -g ondemand
+done
