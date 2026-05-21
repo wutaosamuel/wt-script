c117e4f90b2849f45aec28bc5dafa0102782d4cf
Tao Wu <Tao.Wu@microchip.com>
2026-05-21 13:16:25 +0800

    macos/ad-hoc disable ipv6 network for internet sharing

--- macos/ad-hoc-network.sh
+++ macos/ad-hoc-network.sh
@@ -2,8 +2,10 @@
 
 sudo networksetup -createnetworkservice AdHoc lo0
 sudo networksetup -setmanual AdHoc 192.168.30.88 255.255.255.255
+sudo sysctl -w net.inet6.ip6.forwarding=0
 
 # remove the network configuration files, so that the system can regenerate them fresh at next boot
+#sudo sysctl -w net.inet6.ip6.forwarding=1
 #sudo rm /Library/Preferences/SystemConfiguration/NetworkInterfaces.plist
 #sudo rm /Library/Preferences/SystemConfiguration/preferences.plist
 #sudo reboot
